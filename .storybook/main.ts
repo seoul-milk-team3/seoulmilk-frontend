603d7e8b454cac02a70f90bfd3232efe74d815a7
cindy-chaewon <chaewon1769@daum.net>
2025-02-27 02:43:15 +0900

    #10 fix: index.ts 추가

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -31,7 +31,10 @@ const config: StorybookConfig = {
         "@": path.resolve(__dirname, "../packages/ui/src"),
         "@seoulmilk/ui": path.resolve(__dirname, "../packages/ui/src"),
         "@seoulmilk/utils": path.resolve(__dirname, "../packages/utils/src"),
-        "@seoulmilk/icon": path.resolve(__dirname, "../packages/icon/src"),
+        "@seoulmilk/icon": path.resolve(
+          __dirname,
+          "../packages/icon/src/index.ts"
+        ),
         "@seoulmilk/styles": path.resolve(__dirname, "../packages/styles/src"),
       };
     }
