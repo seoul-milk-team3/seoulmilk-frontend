804f69ec77d5f04f4696d4ae7916c400ddd3620a
cindy-chaewon <chaewon1769@daum.net>
2025-03-02 01:40:18 +0900

    #20 feat: 대리점 라우터 세팅

---
+++ apps/agency/src/pages/list/ListPage.tsx
@@ -0,0 +1,5 @@
+const ListPage = () => {
+  return <h1>자료조회 페이지</h1>;
+};
+
+export default ListPage;
---
+++ apps/agency/src/routes/router.tsx
@@ -0,0 +1,23 @@
+import { lazy } from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import AuthLayout from '@/layouts/auth/AuthLayout';
+import MainLayout from '@/layouts/main/MainLayout';
+
+const Login = lazy(() => import('@/pages/login/LoginPage'));
+const List = lazy(() => import('@/pages/list/ListPage'));
+const FileUpload = lazy(() => import('@/pages/fileUpload/FileUploadPage'));
+
+export const router = createBrowserRouter([
+  {
+    element: <AuthLayout />,
+    children: [{ path: '/login', element: <Login /> }],
+  },
+
+  {
+    element: <MainLayout />,
+    children: [
+      { path: '/', element: <FileUpload /> },
+      { path: '/list', element: <List /> },
+    ],
+  },
+]);
