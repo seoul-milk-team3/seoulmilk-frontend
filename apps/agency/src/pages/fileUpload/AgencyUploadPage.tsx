1cb800292af6dee7ae29104c4fa5ecab778acbb7
yumin <umin030312@naver.com>
2025-03-03 22:31:16 +0900

    feat : 웹캠 촬영 및 업로드 구현

--- apps/agency/src/pages/fileUpload/AgencyUploadPage.tsx
+++ apps/agency/src/pages/fileUpload/AgencyUploadPage.tsx
@@ -1,23 +1,47 @@
 import { pageContainerStyle, uploadContentStyle, uploadBoxStyle } from '@agency/pages/fileUpload/AgencyUploadPage.style';
 import { Flex } from '@seoulmilk/ui';
-import  FileCheck  from '@agency/components/FileUpload/FileCheck/FileCheck';
-import  UploadSection  from '@agency/components/FileUpload/UploadSection/UploadSection';
+import { UploadSection } from '@seoulmilk/ui';
+import MobileUploadSection from '@agency/components/FileUpload/MobileUploadSection/MobileUploadSection';
 import { useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
 
 const AgencyUploadPage = () => {
   const [uploadState, setUploadState] = useState<'idle' | 'uploading' | 'uploaded' | 'checking'>('idle');
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+
+  const handleUploadStart = () => {
+    console.log("Upload started");
+    setUploadState("uploading");
+  };
+
+  const handleUploadSuccess = () => {
+    console.log("Upload successful");
+    setUploadState("uploaded");
+  };
+
+  const handleCheckValidity = () => {
+    console.log("Checking validity");
+    setUploadState("checking");
+  };
 
   return (
     <Flex css={pageContainerStyle}>
       <Flex css={uploadContentStyle}>
         <Flex css={uploadBoxStyle}>
-          <UploadSection
-          />
+          {isMobile ? (
+            <MobileUploadSection 
+            />
+          ) : (
+            <UploadSection 
+              onUploadStart={handleUploadStart}
+              onUploadSuccess={handleUploadSuccess}
+              onCheckValidity={handleCheckValidity}
+            />
+          )}
+        </Flex>
       </Flex>
-    </Flex>
     </Flex>
   );
 };
-
 
 export default AgencyUploadPage;
