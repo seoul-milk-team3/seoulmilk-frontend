e62d90bd5647a8920546a0a4fd64704c2d6297e3
cindy-chaewon <chaewon1769@daum.net>
2025-03-09 22:23:48 +0900

    #66 fix: 페이지네이션 스타일 변경

--- packages/ui/src/Pagination/Pagination.style.ts
+++ packages/ui/src/Pagination/Pagination.style.ts
@@ -11,8 +11,18 @@ export const paginationStyle = css({
 
 export const pageButtonStyle = (isActive: boolean) =>
   css({
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "3.2rem",
+    width: "3.2rem",
     fontSize: "1.6rem",
     fontWeight: isActive ? "700" : "400",
-    color: isActive ? theme.colors.green_60 : theme.colors.grayscale_40,
+    color: isActive ? theme.colors.grayscale_0 : theme.colors.grayscale_40,
     cursor: "pointer",
+
+    ...(isActive && {
+      borderRadius: "6px",
+      background: theme.colors.green_50,
+    }),
   });
