749a76760f0557312bc98d6bff057634914c27bb
cindy-chaewon <chaewon1769@daum.net>
2025-03-13 20:52:36 +0900

    #81 feat: qa 반영

--- apps/main/src/pages/confirm/components/ConfirmItem/ConfirmItem.tsx
+++ apps/main/src/pages/confirm/components/ConfirmItem/ConfirmItem.tsx
@@ -16,7 +16,9 @@ const ConfirmItem = ({ id, date, supplier, isChecked, onCheck }: ConfirmItemProp
       <Text tag="md2-text-medium" css={{ width: '15.6rem', marginLeft: '1.2rem' }}>
         {date}
       </Text>
-      <Text tag="md2-text-medium" css={{ width: '17rem', marginLeft: '1.2rem' }}>
+      <Text
+        tag="md2-text-medium"
+        css={{ width: '17rem', marginLeft: '1.2rem', paddingRight: '2.6rem', textOverflow: 'ellipsis' }}>
         {supplier}
       </Text>
       <Flex styles={{ align: 'center', justify: 'center' }} css={labelBackgroundStyle}>
